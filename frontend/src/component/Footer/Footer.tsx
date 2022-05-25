3d34faeb07fe46a471be01d64245f58a14ce6ce6
Sara Jimenez Carbajal <sara.jimenez@sngular.com>
2022-05-25 18:00:40 +0200

    modified footer component and contacts page

--- frontend/src/component/Footer/Footer.tsx
+++ frontend/src/component/Footer/Footer.tsx
@@ -8,22 +8,23 @@ const Footer: FC = () => {
             <div className="container">
                 <div className="d-flex justify-content-between">
                     <div className="footer-left">
-                        <h3>Perfume</h3>
-                        <p>(066) 696-66-23</p>
-                        <br/>
-                        <p>from 08:00 to 20:00 without breaks and weekends</p>
+                        <p>© Copyright - Sngular QA 2022</p>
                     </div>
                     <div className="footer-right">
                         <h3>Social networks</h3>
-                        <a href="https://www.linkedin.com/in/merikbest/">
+                        <a href="https://www.linkedin.com/company/sngular">
                             <i className="fab fa-linkedin fa-2x mr-3" style={{color: "white"}}></i>
                         </a>
-                        <a href="#"><i className="fab fa-facebook-f fa-2x mr-3" style={{color: "white"}}></i></a>
-                        <a href="#"><i className="fab fa-twitter fa-2x mr-3" style={{color: "white"}}></i></a>
+                        <a href="https://twitter.com/sngular">
+                            <i className="fab fa-twitter fa-2x mr-3" style={{color: "white"}}></i>
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCJDkEoAAZclorR4jEF9Kv3Q">
+                            <i className="fab fa-youtube fa-2x mr-3" style={{color: "white"}}></i>
+                        </a>
+                        <a href="https://qa.sngular.com/es">
+                            <i className="fas fa-globe fa-2x mr-3" style={{color: "white"}}></i>
+                        </a>
                     </div>
-                </div>
-                <div className="mx-auto" style={{width: "200px"}}>
-                    <p>© Copy right merikbest</p>
                 </div>
             </div>
         </footer>
--- frontend/src/pages/Contacts/Contacts.tsx
+++ frontend/src/pages/Contacts/Contacts.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from 'react';
-import {faInfoCircle} from "@fortawesome/free-solid-svg-icons";
+import {faBusinessTime, faInbox, faInfoCircle, faTruck} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Contacts: FC = () => {
@@ -7,14 +7,14 @@ const Contacts: FC = () => {
         <div className="container mt-5">
             <h4><FontAwesomeIcon className="ml-2 mr-2" icon={faInfoCircle}/>Contacts</h4>
             <br/>
-            <p><b>Mobile:</b> (066) 696-66-23<br/>
-                <b>E-mail:</b> merikbest2015@gmail.com</p>
+            <p>
+                <b><FontAwesomeIcon className="ml-2 mr-2" icon={faInbox}/>E-mail:</b> qa.info@sngular.com</p>
             <br/>
-            <h6>Working time</h6>
+            <h4><FontAwesomeIcon className="ml-2 mr-2" icon={faBusinessTime}/>Working time</h4>
             <p>The online store is open from 08:00 to 20:00 without breaks and weekends. <br/>
                 Online orders are accepted around the clock.</p>
             <br/>
-            <h6>Delivery</h6>
+            <h4><FontAwesomeIcon className="ml-2 mr-2" icon={faTruck}/>Delivery</h4>
             <p>Delivery of orders come through courier service.</p>
         </div>
     );
