2fcc609c2ccb5c28148526ddd3d0bf155bf6b987
luislopezgsn <luis.lopezg@sngular.com>
2025-05-21 09:14:50 +0200

    refactored contacts test to adhere to POM

--- cypress/e2e/contacts.cy.js
+++ cypress/e2e/contacts.cy.js
@@ -1,25 +1,23 @@
+import ContactsPage from '../support/pages/contactsPage';
+
 describe('Contacts Page - Dynamic Content Verification', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:3000/contacts');
+  beforeEach(() => {
+    ContactsPage.visit();
+  });
+
+  it('should verify all expected elements exist and contain some content', () => {
+    ContactsPage.getContactsHeader().should('exist').and('have.text', 'Contacts');
+
+    ContactsPage.getEmailLink().should('exist').and(($el) => {
+      expect($el.attr('href')).to.not.be.empty;
     });
-  
-    it('should verify all expected elements exist and contain some content', () => {
-      const elements = [
-        { selector: 'h4:contains("Contacts")', type: 'text' },
-        { selector: '#email', type: 'attribute', attr: 'href' },
-        { selector: 'h4:contains("Working time")', type: 'text' },
-        { selector: 'h4:contains("Delivery")', type: 'text' }
-      ];
-  
-      elements.forEach(({ selector, type, attr }) => {
-        cy.get(selector).should('exist').and(($el) => {
-          if (type === 'text') {
-            expect($el.text().trim()).not.to.be.empty;
-          } else if (type === 'attribute') {
-            expect($el.attr(attr)).not.to.be.empty;
-          }
-        });
-      });
+
+    ContactsPage.getWorkingTimeHeader().should('exist').and(($el) => {
+      expect($el.text().trim()).not.to.be.empty;
+    });
+
+    ContactsPage.getDeliveryHeader().should('exist').and(($el) => {
+      expect($el.text().trim()).not.to.be.empty;
     });
   });
-  
+});
