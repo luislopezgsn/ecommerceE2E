d7919e15ec3537268c55d4a19ffb3760c0e0908f
luislopezgsn <luis.lopezg@sngular.com>
2025-05-29 10:19:15 +0200

    refactor almost ready, broke a test

--- cypress/e2e/perfumesFilters.cy.js
+++ cypress/e2e/perfumesFilters.cy.js
@@ -1,50 +1,35 @@
-import PerfumesFiltersPage from '../support/pages/perfumesFiltersPage';
-import CardsPage from '../support/pages/cardsPage';
+// cypress/e2e/perfumesFilters.cy.js
 
-describe('Menu Page - Brand Selector & Search Filter', () => {
+import perfumesFiltersPage from '../support/pages/perfumesFiltersPage';
+
+describe('Perfume Page Tests', () => {
   beforeEach(() => {
-    Cypress.on('uncaught:exception', () => false);
-    PerfumesFiltersPage.visit();
+    perfumesFiltersPage.visit();
   });
 
-  it('should verify the brand selector exists and has options', () => {
-    PerfumesFiltersPage.getBrandSelector().should('exist').and('be.visible');
-    PerfumesFiltersPage.getBrandOptions().should('have.length.at.least', 2);
+  it('should verify all brand options exist', () => {
+    perfumesFiltersPage.getBrandOptions().should('have.length.at.least', 2);
   });
 
-  it('should filter items when selecting a brand', () => {
-    PerfumesFiltersPage.selectBrandSearchOption();
-    CardsPage.assertCardsExist();
+  it('should search for perfumes by brand', () => {
+    perfumesFiltersPage.selectBrandSearchFilter('Brand');
+    perfumesFiltersPage.getSearchInput().type('Dior');
+    perfumesFiltersPage.clickSearchButton();
+    perfumesFiltersPage.assertCardsContainText('Dior');
   });
 
-  it('should verify the search field exists and is functional', () => {
-    PerfumesFiltersPage.getSearchInput().should('exist').and('be.visible');
+  it('should filter perfumes by brand checkbox', () => {
+    perfumesFiltersPage.filterByBrandCheckbox('chanel');
+    perfumesFiltersPage.assertCardsContainText('Chanel');
   });
 
-  it('should filter results when typing in the search field', () => {
-    PerfumesFiltersPage.getSearchInput().type('Chanel');
-    PerfumesFiltersPage.clickSearchButton();
-    CardsPage.assertCardsContainText('Chanel');
+  it('should filter perfumes by gender', () => {
+    perfumesFiltersPage.filterByGender(['men', 'women']);
+    perfumesFiltersPage.assertCardsContainAnyText(['Homme', 'Femme']);
   });
 
-  it('should filter cards by brand checkbox', () => {
-    PerfumesFiltersPage.clickBrandCheckbox('Dior');
-    CardsPage.assertCardsContainText('Dior');
-  });
-
-  it('should support multi-brand filtering', () => {
-    PerfumesFiltersPage.clickBrandCheckbox('Chanel');
-    PerfumesFiltersPage.clickBrandCheckbox('Dior');
-    CardsPage.assertCardsMatchEitherText('Chanel', 'Dior');
-  });
-
-  it('should allow clicking gender checkboxes', () => {
-    PerfumesFiltersPage.clickGenderCheckbox('male');
-    PerfumesFiltersPage.clickGenderCheckbox('female');
-  });
-
-  it('should filter cards by price radio button', () => {
-    PerfumesFiltersPage.selectPriceRange('3');
-    CardsPage.assertCardPricesInRange(25, 40);
+  it('should filter perfumes by price range', () => {
+    perfumesFiltersPage.filterByPriceRange('0-50');
+    perfumesFiltersPage.assertCardPricesWithin(0, 50);
   });
 });
--- cypress/support/pages/perfumesFiltersPage.js
+++ cypress/support/pages/perfumesFiltersPage.js
@@ -1,46 +1,76 @@
-class PerfumesFiltersPage {
-    visit() {
-      cy.visit('http://localhost:3000/');
-      cy.contains('PERFUMES').should('be.visible').click();
-      cy.wait(3000);
-    }
-  
-    getBrandSelector() {
-      return cy.get('select.form-control', { timeout: 10000 });
-    }
-  
-    getBrandOptions() {
-      return cy.get('select.form-control option');
-    }
-  
-    selectBrandSearchOption(option = 'perfumer') {
-      cy.get('#searchBy', { timeout: 10000 }).select(option);
-    }
-  
-    getSearchInput() {
-      return cy.get('input[placeholder="Search..."]', { timeout: 10000 });
-    }
-  
-    clickSearchButton() {
-      cy.get('button[type="submit"].btn-dark', { timeout: 10000 }).click();
-    }
-  
-    getCards() {
-      return cy.get('.card', { timeout: 10000 });
-    }
-  
-    clickBrandCheckbox(brandId) {
-      cy.get(`i#${brandId}`).first().click({ force: true });
-    }
-  
-    clickGenderCheckbox(genderId) {
-      cy.get(`i#${genderId}`).first().click({ force: true });
-    }
-  
-    selectPriceRange(value = "3") {
-      cy.get(`input[type="radio"][value="${value}"]`).first().click({ force: true });
-    }
+// cypress/support/pages/perfumePage.js
+
+class PerfumePage {
+  visit() {
+    cy.visit('http://localhost:3000/');
+    cy.contains('PERFUMES').should('be.visible').click();
+    cy.wait(3000);
   }
-  
-  export default new PerfumesFiltersPage();
-  
+
+  getBrandSelector() {
+    return cy.get('select.form-control');
+  }
+
+  getBrandOptions() {
+    return this.getBrandSelector().find('option');
+  }
+
+  selectBrandSearchFilter(value) {
+    cy.get('#searchBy').select(value);
+  }
+
+  getSearchInput() {
+    return cy.get('input[placeholder="Search..."]');
+  }
+
+  clickSearchButton() {
+    return cy.get('button[type="submit"].btn-dark').click();
+  }
+
+  filterByBrandCheckbox(brand) {
+    cy.get(`i#${brand}`).first().click({ force: true });
+  }
+
+  filterByGender(genders = []) {
+    genders.forEach((gender) => {
+      cy.get(`i#${gender}`).first().click({ force: true });
+    });
+  }
+
+  filterByPriceRange(value) {
+    cy.get(`input[type="radio"][value="${value}"]`).first().click({ force: true });
+  }
+
+  getPerfumeCards() {
+    return cy.get('.card');
+  }
+
+  assertCardsContainText(text) {
+    this.getPerfumeCards().each(($el) => {
+      cy.wrap($el).should('contain.text', text);
+    });
+  }
+
+  assertCardsContainAnyText(texts = []) {
+    this.getPerfumeCards().each(($el) => {
+      cy.wrap($el).invoke('text').then((text) => {
+        const found = texts.some((t) => text.includes(t));
+        expect(found).to.be.true;
+      });
+    });
+  }
+
+  assertCardPricesWithin(min, max) {
+    this.getPerfumeCards().each(($el) => {
+      cy.wrap($el).invoke('text').then((text) => {
+        const priceMatch = text.match(/(\d+[.,]?\d*) ?€/);
+        if (priceMatch) {
+          const price = parseFloat(priceMatch[1].replace(',', '.'));
+          expect(price).to.be.within(min, max);
+        }
+      });
+    });
+  }
+}
+
+export default new PerfumePage();
